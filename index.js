0f38953f982bb9662523c3056dc7650b903fbcaf
Juan Quino <quinojuan@gmail.com>
2023-05-19 23:01:15 -0300

    routes y controllers

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import "./database/connectdb.js"
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ ok: true });
-});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log("http://localhost:" + PORT));
